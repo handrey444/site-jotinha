0fb7a20189c7a894f3180f64ee9a6b9c2388143e
handrey444 <carvalho.handrey09@aluno.ifce.edu.br>
2023-12-07 16:07:18 -0300

    n\a

--- script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById('btn');
-function meche(){
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active')
-}
-btn.addEventListener('click', meche);
